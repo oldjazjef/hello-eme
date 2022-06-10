d10ac09da3ad499b65b1939a76e66ef7f5f0beee
Emanuel Mistretta <emanuel.mistretta@etrex.ch>
2022-06-10 17:09:04 +0200

    port change

--- server/hello-eme-api/config/env/production/server.js
+++ server/hello-eme-api/config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env('PORT', '8000'),
+  port: env('PORT'),
   app: {
     keys: env.array('APP_KEYS', ["placeholderKey1","placeholderKey1"]),
   },
