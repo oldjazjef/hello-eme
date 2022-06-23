c1f6b20df3aa98be35d947880a77e4ebc5892767
Emanuel Mistretta <emanuel.mistretta@etrex.ch>
2022-06-23 15:49:38 +0200

    port update

--- server/hello-eme-api/config/env/production/server.js
+++ server/hello-eme-api/config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
   url: env('MY_HEROKU_URL'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ["placeholderKey1","placeholderKey1"])
   },
